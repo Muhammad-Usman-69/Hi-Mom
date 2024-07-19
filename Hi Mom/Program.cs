124abecb65ae2034d69ed1adfb7b94fa7bd880ee
Usman <Usman@DESKTOP-6L81EP7>
2024-07-19 09:59:13 +0500

    Add project files.

---
+++ Hi Mom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hi_Mom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a single line comment
+            /*
+             this is a multi-line comment
+             */
+
+            //Console.WriteLine("Hi Mom!");
+            //Console.Write(69);
+            //Console.WriteLine("Hi Usman!");
+
+            //declaring variable
+            //int num = 420;
+            //Console.WriteLine("My number is " + num);
+
+            /* Data type are:
+             Integar - int num = 12;
+             Floating Point Number - float this;
+             Charactr - char x = 'X';
+             Boolean - bool isHardworking = true;
+             String - string name = "Usman";
+             */
+
+            //taking input by reading line
+            string inp = Console.ReadLine();
+            Console.WriteLine(inp);
+
+            //stops the console from closing
+            Console.ReadLine();
+        }
+    }
+}
