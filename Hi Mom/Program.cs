375e865473f3899505db061e3b8da27a6a7aefcc
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-07-19 12:29:21 +0500

    type casting - implicit and explicit

--- Hi Mom/Program.cs
+++ Hi Mom/Program.cs
@@ -26,20 +26,43 @@ namespace Hi_Mom
             /* Data type are:
              Integar - int num = 12; --> 4 bytes
              Long - long num = 12; --> 8 bytes
-             Floating Point Number - float this; --> 4 bytes
-             Double - double that; --> 8 bytes
+             Floating Point Number - float thisF; --> 4 bytes
+             Double - double thatD; --> 8 bytes
              Charactr - char x = 'X'; --> 2 bytes
              Boolean - bool isHardworking = true; --> 1 bit
              String - string name = "Usman"; --> 2 bytes per character
              */
 
-            //writing info for taking name
-            Console.WriteLine("What's your name?");
-            Console.Write("Name: ");
+            // for float, writing F is neccesary after decimal number 
+            // for double, writing D is optional
+            // bonus: C# is a typesafe language which means a variable can't change its type across the code. An int variable can't be assigned a string
 
-            //taking input by reading line
-            string inp = Console.ReadLine();
-            Console.WriteLine("Your name is " + inp + ".");
+            /* Example of taking input
+             //writing info for taking name
+             Console.WriteLine("What's your name?");
+             Console.Write("Name: ");
+             
+             //taking input by reading line
+             string inp = Console.ReadLine();
+             Console.WriteLine("Your name is " + inp + ".");
+            */
+
+            //Type Casting - conversion of variable type to another - Its types are:
+
+            // 1) Implicit Casting - handled automatically by the compiler - a promotion table for char is following
+            // char to int to long to float to double
+            /*
+             char x = 'x';
+             int integar = x; //converted to integar following the promotion table
+             Console.WriteLine(integar);
+            */
+
+            // 2) Explicit Casting - handled manually by the programmer
+            /*
+             int integar = (int) 'x'; //converted to integar
+             Console.WriteLine(integar);
+            */
+
 
             //stops the console from closing
             Console.ReadLine();
